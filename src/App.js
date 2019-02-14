29d58a87968c3dee876aa72a129adbb522ccabde
FeronCastor <ferocastor@gmail.com>
2019-02-14 23:55:00 +0700

    changed the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Calculator from './components/Calculator/Calculator'
 import Slider from './components/Slider/Slider'
 import bmw from './components/Slider/picture/bmw.jpg';
 import ferrari from './components/Slider/picture/ferrari.jpg';
@@ -14,9 +13,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
         <Slider images={images}/>
-        <Calculator />
+        
       </div>
     );
   }
